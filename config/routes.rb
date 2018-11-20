0b1e43d8f25d7eed0b4ca55eeb506b0a4d81a4f5
Sylvain Tersoglio <sylvain.tersoglio@gmail.com>
2018-11-20 12:03:10 +0100

    controller sapces index show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  # get 'spaces/index'
+  # get 'spaces/show'
+  # get 'spaces/new'
+  # get 'spaces/create'
+  # get 'spaces/edit'
+  # get 'spaces/update'
+  # get 'spaces/detroy'
+  resources :spaces
   devise_for :users
-  root to: 'pages#home'
+  root to: 'spaces#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
