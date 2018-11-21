7c6b33f7eccbfbc0dcb8f426dd508a16f51bd480
Paul de Castelbajac <paul.d.c@hotmail.fr>
2018-11-21 13:55:24 +0100

    controller show and profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   resources :spaces do
-    resources :bookings, only: [:create]
+    resources :bookings, only: [:create, :index, :show]
   end
   devise_for :users
 
+  namespace :profile do
+    resources :bookings, only: [:index]
+  end
+
   root to: 'spaces#index'
 end
+
+
