9e113a8d880fcce7345a17fd2def2e4084675c87
Sylvain Tersoglio <sylvain.tersoglio@gmail.com>
2018-11-22 14:55:07 +0100

    CSS cards + Landing page banner

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-addImageToSpaces
 ActiveRecord::Schema.define(version: 2018_11_22_101717) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
