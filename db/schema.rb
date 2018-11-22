7d6d1d4217405c16d3befdcc912ac2d019a99567
Paul de Castelbajac <paul.d.c@hotmail.fr>
2018-11-22 12:56:38 +0100

    working on map

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_131558) do
+ActiveRecord::Schema.define(version: 2018_11_21_153854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,9 @@ ActiveRecord::Schema.define(version: 2018_11_21_131558) do
     t.integer "number_of_seats"
     t.string "title"
     t.boolean "available", default: true
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
     t.index ["owner_id"], name: "index_spaces_on_owner_id"
   end
 
